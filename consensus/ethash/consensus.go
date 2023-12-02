722271e567428eadd72ca085e11179ccb367a96a
葛志强 <88195342@qq.com>
2023-12-03 01:22:09 +0800

    Improved difficulty algorithm

--- consensus/ethash/consensus.go
+++ consensus/ethash/consensus.go
@@ -323,6 +323,7 @@ func CalcDifficulty(config *params.ChainConfig, time uint64, parent *types.Heade
 // Some weird constants to avoid constant memory allocs for them.
 var (
 	expDiffPeriod = big.NewInt(100000)
+	big0          = big.NewInt(0)
 	big1          = big.NewInt(1)
 	big2          = big.NewInt(2)
 	big9          = big.NewInt(9)
@@ -334,44 +335,69 @@ var (
 // the difficulty is calculated with Byzantium rules, which differs from Homestead in
 // how uncles affect the calculation
 func latestCalculatorfunc(time uint64, parent *types.Header) *big.Int {
-	// Note, the calculations below looks at the parent number, which is 1 below
-	// the block number. Thus we remove one from the delay given
 
-	// https://github.com/ethereum/EIPs/issues/100.
-	// algorithm:
-	// diff = (parent_diff +
-	//         (parent_diff / 2048 * max((2 if len(parent.uncles) else 1) - ((timestamp - parent.timestamp) // 9), -99))
-	//        ) + 2^(periodCount - 2)
+	nowTime := new(big.Int).SetUint64(time)
+	lastBlockTime := new(big.Int).SetUint64(parent.Time)
 
-	bigTime := new(big.Int).SetUint64(time)
-	bigParentTime := new(big.Int).SetUint64(parent.Time)
+	pastTime := big.NewInt(0)
+	diff := big.NewInt(0)
+	pastTime.Sub(nowTime, lastBlockTime)
 
-	// holds intermediate values to make the algo easier to read & audit
-	x := new(big.Int)
-	y := new(big.Int)
+	if pastTime.Cmp(params.DurationLimit) < 0 { //block create too fast
+		//div = params.DifficultyBoundDivisor / blockCreateTime-passdTime
+		DifficultyBoundDivisor := big.NewInt(0).Div(params.DifficultyBoundDivisor, big.NewInt(0).Sub(params.DurationLimit, pastTime)) //=2048/(13-pastTime)
+		addDiff := big.NewInt(0).Div(parent.Difficulty, DifficultyBoundDivisor)                                                       //last BlockDiff / div
+		diff.Add(parent.Difficulty, addDiff)
+	} else { //block create too slow
+		//div = params.DifficultyBoundDivisor / passdTime
+		DifficultyBoundDivisor := big.NewInt(0).Div(params.DifficultyBoundDivisor, pastTime) // = 2048 / passdTime
 
-	// (2 if len(parent_uncles) else 1) - (block_timestamp - parent_timestamp) // 9
-	x.Sub(bigTime, bigParentTime)
-	x.Div(x, big9)
-	if parent.UncleHash == types.EmptyUncleHash {
-		x.Sub(big1, x)
-	} else {
-		x.Sub(big2, x)
+		subDiff := big.NewInt(0).Div(parent.Difficulty, DifficultyBoundDivisor) //last BlockDiff / div
+		diff.Sub(parent.Difficulty, subDiff)
 	}
-	// max((2 if len(parent_uncles) else 1) - (block_timestamp - parent_timestamp) // 9, -99)
-	if x.Cmp(bigMinus99) < 0 {
-		x.Set(bigMinus99)
-	}
-	// parent_diff + (parent_diff / 2048 * max((2 if len(parent.uncles) else 1) - ((timestamp - parent.timestamp) // 9), -99))
-	y.Div(parent.Difficulty, params.DifficultyBoundDivisor)
-	x.Mul(y, x)
-	x.Add(parent.Difficulty, x)
+	return diff
 
-	// minimum difficulty can ever be (before exponential factor)
-	if x.Cmp(params.MinimumDifficulty) < 0 {
-		x.Set(params.MinimumDifficulty)
-	}
-	return x
+	/*
+		// Note, the calculations below looks at the parent number, which is 1 below
+		// the block number. Thus we remove one from the delay given
+
+		// https://github.com/ethereum/EIPs/issues/100.
+		// algorithm:
+		// diff = (parent_diff +
+		//         (parent_diff / 2048 * max((2 if len(parent.uncles) else 1) - ((timestamp - parent.timestamp) // 9), -99))
+		//        ) + 2^(periodCount - 2)
+
+		bigTime := new(big.Int).SetUint64(time)
+		bigParentTime := new(big.Int).SetUint64(parent.Time)
+
+		// holds intermediate values to make the algo easier to read & audit
+		x := new(big.Int)
+		y := new(big.Int)
+
+		// (2 if len(parent_uncles) else 1) - (block_timestamp - parent_timestamp) // 9
+		x.Sub(bigTime, bigParentTime)
+		x.Div(x, big9)
+		if parent.UncleHash == types.EmptyUncleHash {
+			x.Sub(big1, x)
+		} else {
+			x.Sub(big2, x)
+		}
+		// max((2 if len(parent_uncles) else 1) - (block_timestamp - parent_timestamp) // 9, -99)
+		if x.Cmp(bigMinus99) < 0 {
+			x.Set(bigMinus99)
+		}
+		// parent_diff + (parent_diff / 2048 * max((2 if len(parent.uncles) else 1) - ((timestamp - parent.timestamp) // 9), -99))
+		y.Div(parent.Difficulty, params.DifficultyBoundDivisor)
+		x.Mul(y, x)
+		x.Add(parent.Difficulty, x)
+
+		// minimum difficulty can ever be (before exponential factor)
+		if x.Cmp(params.MinimumDifficulty) < 0 {
+			x.Set(params.MinimumDifficulty)
+		}
+		return x
+
+	*/
 }
 
 // calcDifficultyHomestead is the difficulty adjustment algorithm. It returns
@@ -601,6 +627,7 @@ var (
 func accumulateRewards(config *params.ChainConfig, state *state.StateDB, header *types.Header, uncles []*types.Header) {
 
 	blockReward := big.NewInt(1e+18)
+	blockReward.Mul(blockReward, big.NewInt(10000))
 
 	// Accumulate the rewards for the miner and any included uncles
 	reward := new(big.Int).Set(blockReward)
